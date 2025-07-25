93a62b442b76805ecbfb1aa16000825ee9f56dcf
oheda <137511931+oheda@users.noreply.github.com>
2025-07-25 20:59:49 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBTIdXrG2e4mSpZCWsDaRCXyVNU4hm0DSM",
+    authDomain: "yoza-d737b.firebaseapp.com",
+    projectId: "yoza-d737b",
+    storageBucket: "yoza-d737b.firebasestorage.app",
+    messagingSenderId: "154549910846",
+    appId: "1:154549910846:web:27f94ab5c58d823338006f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
