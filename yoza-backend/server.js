603e465d576ac2c70ac8a3bef3d74046935f2080
ozemi <137511931+oheda@users.noreply.github.com>
2025-08-05 00:24:16 +0530

    Add files via upload

--- yoza-backend/server.js
+++ yoza-backend/server.js
@@ -21,10 +21,7 @@ app.use("/api", authRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(process.env.MONGO_URI);
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => {
